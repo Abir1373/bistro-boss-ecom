db1190d24eb0ef6db6a61a96833a490947643db2
Abir1373 <abirm3188@gmail.com>
2023-11-01 01:35:10 +0600

    1.11.23

--- Client/src/app/(Dashboard)/layout.jsx
+++ Client/src/app/(Dashboard)/layout.jsx
@@ -12,9 +12,9 @@ import { BsBookmarksFill } from "react-icons/bs";
 
 import { ImMenu } from "react-icons/im";
 
-import loadActiveUser from '../pages/utils/loadActiveUser';
-
 import { useAuth } from '../providers/AuthProvider';
+import { loadActiveUser } from '../pages/utils/loadActiveUser';
+import { useEffect, useState } from 'react';
 
 
 
@@ -22,66 +22,67 @@ import { useAuth } from '../providers/AuthProvider';
 
 
 const DashboardLayout = ({ children }) => {
-    let email = '';
-    let roleinfo = 'user'
+
+
     const { user } = useAuth();
-    // console.log(user)
+    const [roleinfo, setRoleInfo] = useState(null);
 
-    if (user === null) return <span className="loading loading-spinner loading-lg"></span>
-    else if (user !== null) { console.log(user.email) }
-    let [data] = loadActiveUser(user.email)
-    if (data === undefined) return <span className="loading loading-spinner loading-lg"></span>
-    else {
-        console.log(data)
-        roleinfo = data.user_role
-        return (
+    useEffect(() => {
+        if (user) {
+            loadActiveUser(user.email)
+                .then(data => setRoleInfo(data))
+                .catch(error => console.error('Error fetching role info:', error));
+        }
+    }, [user]);
 
-            <div className="drawer lg:drawer-open ">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
-                    {/* Page content here */}
-                    {children}
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden ">Open drawer</label>
-                </div>
-                <div className="drawer-side ">
-                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                    {
-                        roleinfo === "user" && <ul className="menu p-4 w-80 min-h-full  text-base-content  " style={{ backgroundColor: '#D1A054' }}>
-                            {/* Sidebar content here */}
-                            <li className='mt-4'><Link href="/userhome" className="text-2xl uppercase">  <FaHome></FaHome> <span> user home </span></Link></li>
-                            <li className='mt-4'><Link href="/reservation" className="text-2xl uppercase">  <FaCalendarWeek></FaCalendarWeek> <span> reservation </span> </Link></li>
-                            <li className='mt-4'><Link href="/paymenthistory" className="text-2xl uppercase">  <FaWallet></FaWallet> <span> payment history </span> </Link></li>
-                            <li className='mt-4'><Link href="/cartdashboard" className="text-2xl uppercase">  <FaCartPlus></FaCartPlus> <span> my cart </span></Link></li>
-                            <li className='mt-4'><Link href="/addreview" className="text-2xl uppercase">  <MdFeedback></MdFeedback> <span> add review </span></Link></li>
-                            <li className='mt-4'><Link href="/mybooking" className="text-2xl uppercase">  <BsBookmarksFill></BsBookmarksFill> <span> my booking </span></Link></li>
-                            
-                            <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <FaHome></FaHome> <span> home </span></Link></li>
-                            <li className='mt-4'><Link href="/menu" className="text-2xl uppercase">  <ImMenu></ImMenu> <span> menu </span></Link></li>
-                            <li className='mt-4'><Link href="/shop" className="text-2xl uppercase">  <FaBagShopping></FaBagShopping> <span> shop </span></Link></li>
-                            <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <MdMarkEmailRead></MdMarkEmailRead> <span> contact </span></Link></li>
-                        </ul>
-                    }
-                    {
-                        roleinfo === "admin" && <ul className="menu p-4 w-80 min-h-full  text-base-content  " style={{ backgroundColor: '#D1A054' }}>
-                            {/* Sidebar content here */}
-                            <li className='mt-4'><Link href="/adminhome" className="text-2xl uppercase">  <FaHome></FaHome> <span> admin home </span></Link></li>
-                            <li className='mt-4'><Link href="/additems" className="text-2xl uppercase">  <FaCalendarWeek></FaCalendarWeek> <span> add items </span> </Link></li>
-                            <li className='mt-4'><Link href="/updateitem" className="text-2xl uppercase">  <FaCalendarWeek></FaCalendarWeek> <span> update items </span> </Link></li>
-                            <li className='mt-4'><Link href="/manageitems" className="text-2xl uppercase">  <FaWallet></FaWallet> <span> manage items </span> </Link></li>
-                            <li className='mt-4'><Link href="/managebookings" className="text-2xl uppercase">  <FaCartPlus></FaCartPlus> <span> manage bookings </span></Link></li>
-                            <li className='mt-4'><Link href="/allusers" className="text-2xl uppercase">  <MdFeedback></MdFeedback> <span> all users </span></Link></li>
-                            
-                            <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <FaHome></FaHome> <span> home </span></Link></li>
-                            <li className='mt-4'><Link href="/menu" className="text-2xl uppercase">  <ImMenu></ImMenu> <span> menu </span></Link></li>
-                            <li className='mt-4'><Link href="/shop" className="text-2xl uppercase">  <FaBagShopping></FaBagShopping> <span> shop </span></Link></li>
-                            <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <MdMarkEmailRead></MdMarkEmailRead> <span> contact </span></Link></li>
-                        </ul>
-                    }
+    return (
 
-                </div>
+        <div className="drawer lg:drawer-open ">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col items-center justify-center">
+                {/* Page content here */}
+                {children}
+                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden ">Open drawer</label>
             </div>
-        );
-    }
+            <div className="drawer-side ">
+                <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                {
+                    
+                    roleinfo === "user" && <ul className="menu p-4 w-80 min-h-full  text-base-content  " style={{ backgroundColor: '#D1A054' }}>
+                        {/* Sidebar content here */}
+                        <li className='mt-4'><Link href="/userhome" className="text-2xl uppercase">  <FaHome></FaHome> <span> user home </span></Link></li>
+                        <li className='mt-4'><Link href="/reservation" className="text-2xl uppercase">  <FaCalendarWeek></FaCalendarWeek> <span> reservation </span> </Link></li>
+                        <li className='mt-4'><Link href="/paymenthistory" className="text-2xl uppercase">  <FaWallet></FaWallet> <span> payment history </span> </Link></li>
+                        <li className='mt-4'><Link href="/cartdashboard" className="text-2xl uppercase">  <FaCartPlus></FaCartPlus> <span> my cart </span></Link></li>
+                        <li className='mt-4'><Link href="/addreview" className="text-2xl uppercase">  <MdFeedback></MdFeedback> <span> add review </span></Link></li>
+                        <li className='mt-4'><Link href="/mybooking" className="text-2xl uppercase">  <BsBookmarksFill></BsBookmarksFill> <span> my booking </span></Link></li>
+
+                        <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <FaHome></FaHome> <span> home </span></Link></li>
+                        <li className='mt-4'><Link href="/menu" className="text-2xl uppercase">  <ImMenu></ImMenu> <span> menu </span></Link></li>
+                        <li className='mt-4'><Link href="/shop" className="text-2xl uppercase">  <FaBagShopping></FaBagShopping> <span> shop </span></Link></li>
+                        <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <MdMarkEmailRead></MdMarkEmailRead> <span> contact </span></Link></li>
+                    </ul>
+                }
+                {
+                    roleinfo === "admin" && <ul className="menu p-4 w-80 min-h-full  text-base-content  " style={{ backgroundColor: '#D1A054' }}>
+                        {/* Sidebar content here */}
+                        <li className='mt-4'><Link href="/adminhome" className="text-2xl uppercase">  <FaHome></FaHome> <span> admin home </span></Link></li>
+                        <li className='mt-4'><Link href="/additems" className="text-2xl uppercase">  <FaCalendarWeek></FaCalendarWeek> <span> add items </span> </Link></li>
+                        <li className='mt-4'><Link href="/updateitem" className="text-2xl uppercase">  <FaCalendarWeek></FaCalendarWeek> <span> update items </span> </Link></li>
+                        <li className='mt-4'><Link href="/manageitems" className="text-2xl uppercase">  <FaWallet></FaWallet> <span> manage items </span> </Link></li>
+                        <li className='mt-4'><Link href="/managebookings" className="text-2xl uppercase">  <FaCartPlus></FaCartPlus> <span> manage bookings </span></Link></li>
+                        <li className='mt-4'><Link href="/allusers" className="text-2xl uppercase">  <MdFeedback></MdFeedback> <span> all users </span></Link></li>
+
+                        <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <FaHome></FaHome> <span> home </span></Link></li>
+                        <li className='mt-4'><Link href="/menu" className="text-2xl uppercase">  <ImMenu></ImMenu> <span> menu </span></Link></li>
+                        <li className='mt-4'><Link href="/shop" className="text-2xl uppercase">  <FaBagShopping></FaBagShopping> <span> shop </span></Link></li>
+                        <li className='mt-4'><Link href="/" className="text-2xl uppercase">  <MdMarkEmailRead></MdMarkEmailRead> <span> contact </span></Link></li>
+                    </ul>
+                }
+
+            </div>
+        </div>
+    );
 
 };
 
--- Client/src/app/pages/utils/loadActiveUser.jsx
+++ Client/src/app/pages/utils/loadActiveUser.jsx
@@ -1,22 +1,7 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-
-
-const loadActiveUser = (email) => {
-    let [data, setData] = useState([])
-    useEffect(() => {
-        fetch(`http://localhost:5000/users?email=${email}`,{
-            cache:'force-cache'
-        })
-            .then(res => res.json())
-            .then(data => setData(data))
-    }, [])
-    return data
-
-};
-
-export default loadActiveUser;
-
-
-
-
+export const loadActiveUser = async (email) => {
+    // console.log(email)
+    const res = await fetch(`http://localhost:5000/users?email=${email}`)
+    const data = await res.json()
+    // console.log(data)
+    return data[0].user_role
+}
